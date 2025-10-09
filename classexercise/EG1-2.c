903bdd1094bcce2579cc600a15d3adafdb7ba58a
Timsuo <xu.wk@outlook.com>
2025-10-09 14:22:09 +0800

    2025年10月9日 课堂练习

---
+++ classexercise/EG1-2.c
@@ -0,0 +1,25 @@
+//输入一个三位数，使其输出为逆序
+
+
+#include <stdio.h>
+
+int main() {
+    int num, reversed;
+    
+    printf("请输入一个三位整数：");
+    scanf("%d", &num);
+    
+    if (num < 100 || num > 999) {
+        printf("请输入一个有效的三位整数！\n");
+        return 1;
+    }
+    
+    reversed = (num % 10) * 100;    // 获取个位数并放到百位
+    reversed += (num / 10 % 10) * 10; // 获取十位数并放到十位
+    reversed += num / 100;           // 获取百位数并放到个位
+    
+    printf("原数字：%d\n", num);
+    printf("逆序数字：%d\n", reversed);
+    
+    return 0;
+}
