e8c97cf635774d1fda07689a76fa40c58b5abd8f
Timsuo <xu.wk@outlook.com>
2025-10-14 16:23:12 +0800

    2025年10月14日

---
+++ classexercise/int_size.c
@@ -0,0 +1,20 @@
+/*
+2025年9月29日 测试float,int变量大小程序
+*/
+
+#include <stdio.h>
+#include <float.h>
+ 
+int main()
+{
+   printf("int 存储最大字节数 : %lu \n", sizeof(int));
+
+
+
+   printf("float 存储最大字节数 : %lu \n", sizeof(float));
+   printf("float 最小值: %E\n", FLT_MIN );
+   printf("float 最大值: %E\n", FLT_MAX );
+   printf("精度值: %d\n", FLT_DIG );
+   
+   return 0;
+}
